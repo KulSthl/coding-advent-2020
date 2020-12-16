088c18fb9680d7c6d0fe28aa8174fb1df5629d71
KulSthl <hoef.stef@pm.me>
2020-12-16 16:40:38 +0100

    day15👩‍🌾

--- day15/main.go
+++ day15/main.go
@@ -8,7 +8,7 @@ func main() {
 	var const_test = []int{0,3,6};
 	_ = const_test;
 	_ = const_data;
-	var memory = []int{};
+	var memory = [][2]int{};
 	var data = const_data;
 	data, memory = initialize(data,memory)
 	var spoken = 0;
@@ -20,36 +20,35 @@ func main() {
 	fmt.Println(data)
 	// fmt.Println(memory)
 }
-func playGame(data []int ,memory []int)([]int,[]int, int){
+func playGame(data []int ,memory [][2]int)([]int,[][2]int, int){
 	var spoken = 0;
 	var target = data[len(data)-1];
 	var isInMemory = false;
-	for _,integer := range memory{
-		if target == integer {
+	var previous = -1;
+	for idx,integer := range (memory){
+		if target == integer[1] {
+			previous = integer[0];
+			update := [2]int{len(data)-1,target}
+			memory[idx] = update
 			isInMemory = true;
+			break;
 		}
 	}
 	if isInMemory {
-		var previous = -1;
-		var last = -1;
-		for index,integer := range data{
-			if integer == target {
-				previous = last;
-				last = index;
-			}
-		}	
-		spoken := last-previous
+		spoken := len(data)-1-previous
 		data = append(data,spoken);
 	} else {
-		memory = append(memory, target);
+		var temp = [2]int{len(data)-1,target}
+		memory = append(memory, temp);
 		data = append(data, 0);
 	}
 	return data ,memory ,spoken
 }
-func initialize(data []int ,memory []int)([]int, []int){
+func initialize(data []int ,memory [][2]int)([]int, [][2]int){
 	var mem = memory;
-	for _,integer := range data{
-		mem = append(mem,integer);
+	for idx,integer := range data{
+		var temp = [2]int{idx,integer}
+		mem = append(mem,temp);
 	}
 	data = append(data,0);
 	return data, mem;
